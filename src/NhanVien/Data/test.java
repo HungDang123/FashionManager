359d189692d56084e914b9fbcb4fcbdcb9e7c6ec
hnhut <hnhut@HoMinhNhut>
2023-11-13 21:19:29 +0700

    aaaaa

---
+++ src/NhanVien/Data/test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NhanVien.Data;
+
+/**
+ *
+ * @author hnhut
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println(new nhanVien_data_DAO().selectNhanVien_byID_almost("n"));;
+    }
+}
--- src/main/java/com/pro1041/datechooser/EventDateChooser.java
+++
@@ -1,6 +0,0 @@
-package com.pro1041.datechooser;
-
-public interface EventDateChooser {
-
-    public void dateSelected(SelectedAction action, SelectedDate date);
-}
