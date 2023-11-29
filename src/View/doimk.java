89914982878492c1b3384779fcd1478eb6ed8870
Lenovo <Lenovo@NguyenAnhTai2004>
2023-11-29 18:03:29 +0700

    tai

---
+++ src/View/doimk.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class doimk {
+      private String maNhanVien;
+      private String tenNhanVien;
+      private String passworhientai;
+      private String  passwordmoi;
+      private String nhaplaipasswordmoi;
+
+    public doimk() {
+    }
+
+    public doimk(String maNhanVien, String tenNhanVien, String passworhientai, String passwordmoi, String nhaplaipasswordmoi) {
+        this.maNhanVien = maNhanVien;
+        this.tenNhanVien = tenNhanVien;
+        this.passworhientai = passworhientai;
+        this.passwordmoi = passwordmoi;
+        this.nhaplaipasswordmoi = nhaplaipasswordmoi;
+    }
+
+    public String getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(String maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    public String getTenNhanVien() {
+        return tenNhanVien;
+    }
+
+    public void setTenNhanVien(String tenNhanVien) {
+        this.tenNhanVien = tenNhanVien;
+    }
+
+    public String getPassworhientai() {
+        return passworhientai;
+    }
+
+    public void setPassworhientai(String passworhientai) {
+        this.passworhientai = passworhientai;
+    }
+
+    public String getPasswordmoi() {
+        return passwordmoi;
+    }
+
+    public void setPasswordmoi(String passwordmoi) {
+        this.passwordmoi = passwordmoi;
+    }
+
+    public String getNhaplaipasswordmoi() {
+        return nhaplaipasswordmoi;
+    }
+
+    public void setNhaplaipasswordmoi(String nhaplaipasswordmoi) {
+        this.nhaplaipasswordmoi = nhaplaipasswordmoi;
+    }
+      
+      
+}
